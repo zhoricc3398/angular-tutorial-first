85b81c53eb78112025088e9e2f7ab672cadc489d
zhoricc3398 <16200126@ibsu.edu.ge>
2019-07-08 20:56:47 +0400

    feat(3-forms-taks-1): Finish 3-forms-taks-1

--- src/app/register-user/register-user.component.ts
+++ src/app/register-user/register-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register-user',
@@ -11,23 +11,36 @@ export class RegisterUserComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {
     this.registerForm = formBuilder.group({
       email: ['', [Validators.email]],
-      password: ['', [Validators.minLength(8), Validators.pattern(/[a-bA-B0-9]{8,}/)]],
-      confpassword: ['', [Validators.minLength(8), Validators.pattern(/[a-bA-B0-9]/)]],
-      nickname: ['', [Validators.pattern((/[a-bA-B0-9\-]+/))]],
-      phone: ['', [Validators.pattern((/^\+995[0-9]{9}/))]],
-      web: ['', [Validators.pattern(/(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)]]
+      password: ['', [Validators.minLength(8), Validators.pattern(/[\w0-9]{8,}/)]],
+      confpassword: ['', [Validators.minLength(8), Validators.pattern(/[\w0-9]{8,}/)]],
+      nickname: ['', [Validators.pattern(/[\w0-9\-]+/)]],
+      phone: ['', [Validators.pattern(/^\+995[0-9]{9}/)]],
+      web: ['', [Validators.pattern(/(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)]],
+      check: ['', this.checkboxCheck]
+    }, {
+      validators: this.crossPassword
     })
   }
 
   ngOnInit() {
   }
 
-  onSubmit(value) {
-    console.log(value)
-    this.registerForm.reset();
+  crossPassword(formGroup) {
+    const password = formGroup.get('password').value;
+    const confpassword = formGroup.get('confpassword').value;
+
+    return password === confpassword ? null : { passwordStatus: true }
+  }
+
+  checkboxCheck(check) {
+    return check.value ? null : {};
   }
 
   get name() {
-    return this.registerForm.get('name') as FormControl;
+    return this.registerForm.controls;
+  }
+
+  get disableButton() {
+    return this.registerForm.invalid; 
   }
 }
