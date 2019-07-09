85b738447ca339cc6d1e199232fadc9e789eb936
zhoricc3398 <16200126@ibsu.edu.ge>
2019-07-09 19:22:17 +0400

    feat(16-observable-rxjs-part-2): Finish 16-observable-rxjs-part-2

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { data } from './rates';
-import { from } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,7 @@ export class CurrencyService {
   constructor() {
     this.result = this.transformObjecttoArray(data.rates);
 
-    this.Observer = from(this.result)
+    this.Observer = new Observable(this.subscribe())
   }
 
   transformObjecttoArray(object) {
@@ -31,20 +31,22 @@ export class CurrencyService {
     return result;
   }
 
-  subscribe(next, complete) {
-    let i = 0;
-    console.log('it\'s out method');
-
-    for (const item of this.result) {
+  subscribe() {
+    return (subscriber) => {
+      let i = 0;
+      
+  
+      for (const item of this.result) {
+        setTimeout(() => {
+          subscriber.next(item);
+        }, i * 500);
+  
+        i++;
+      }
+  
       setTimeout(() => {
-        next(item);
+        subscriber.complete();
       }, i * 500);
-
-      i++;
     }
-
-    setTimeout(() => {
-      complete();
-    }, i * 500);
   }
 }
