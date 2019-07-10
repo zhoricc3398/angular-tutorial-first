b1d76a9ab2ca5755e87104386efd185747bcc561
zhoricc3398 <16200126@ibsu.edu.ge>
2019-07-10 20:56:36 +0400

    feat(4-observables-and-rxjs): Finish task-1

--- src/app/currency-exchange/currency-exchange.component.ts
+++ src/app/currency-exchange/currency-exchange.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { data } from '../currencies';
+import { FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-currency-exchange',
@@ -6,11 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./currency-exchange.component.scss']
 })
 export class CurrencyExchangeComponent implements OnInit {
+  currencies;
+  exchangeForm;
+  Observer;
+  base;
+  symbol;
+  baseInput;
+  symbolInput;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.currencies = data;
+    this.Observer = new Observable(this.changeValue);
+    this.exchangeForm = formBuilder.group({
+      currency1: [],
+      currency2: []
+    })
+    this.exchangeForm.get('currency1').valueChanges.subscribe(() => this.changeValue('base'));
   }
 
   ngOnInit() {
   }
 
+  selectCurrency(event, type) {
+    if (type === 'base') {
+      this.base = event.target.value;
+    }
+    if (type === 'symbol') {
+      this.symbol = event.target.value;
+    }
+  }
+  
+  changeValue(input) {
+    if (this.base && this.symbol) {
+      let url = '';
+      if (input === 'base') {
+        this.baseInput = this.exchangeForm.get('currency1').value;
+        url = `https://api.exchangeratesapi.io/latest?base=${this.base}&symbols=${this.symbol}`;
+      }
+      this.http.get(url).subscribe(value => {
+        this.update(value, input);
+      });
+    }
+  }
+
+  update(value, input) {
+    if ( input === 'base' ) {
+      this.symbolInput = (this.baseInput || 1) * value.rates[this.symbol];
+    }
+  }
 }
